ca71d87ee19e5d9ae2a2eb2722cef07fe3ac501b
Gianmattia Gherardi <giamalfred@yahoo.it>
2018-09-20 14:54:35 +0200

    Commit

--- js/views/rentals.js
+++ js/views/rentals.js
@@ -12,7 +12,8 @@ var videosDataTableOptions = {
         { data: "casa_produttrice_nome" },
         { data: "regista_nome" },
         { data: "regista_cognome" },
-        { data: "cast.0" },        
+        {   data: "cast"},
+     
         { data: "copie_noleggiate" },
         { data: "copie_danneggiate" },
         {
@@ -33,8 +34,8 @@ var videosDataTableOptions = {
         searchable: false
     }],
     buttons: [
-        { extend: 'copy', text: "Copia" }
-        // { text: "Nuovo dipendente", action: insertEmployee },
+        { extend: 'copy', text: "Copia" },
+        { text: "Noleggia film", action: rentVideo }
         // { extend: 'selectedSingle', text: "Resetta password", action: resetPassword },
         // { extend: 'selectedSingle', text: "Modifica account", action: editEmployee },
         // { extend: 'selectedSingle', text: "Cancella account", action: deleteEmployee }
@@ -59,13 +60,15 @@ function getVideosInStorageWithCountSuccess(data) {
     html +=         BuildVideosTableHead();
     html +=        `<tbody>`;            
     for(var i = 0; i < videos.length; i++) {
-        var oCell = formatAvailableCopiesCellObject(videos[i])
+        var oAvailCopiesCell = formatAvailableCopiesCellObject(videos[i])
+        var castCellRender = formatCastCell(videos[i].cast); 
+        videos[i].cast = castCellRender;
             html +=     `<tr>
                             <td>${videos[i].id_film}</td>
                             <td>${videos[i].casa_produttrice_nome}</td>                             
                             <td>${videos[i].regista_nome}</td>   
                             <td>${videos[i].regista_cognome}</td>  
-                            <td>${videos[i].cast}</td>                   
+                            <td>${videos[i].cast}</td>    
                             <td>${videos[i].copie_noleggiate}</td>
                             <td>${videos[i].copie_danneggiate}</td>                            
                             <td></td>
@@ -73,7 +76,7 @@ function getVideosInStorageWithCountSuccess(data) {
                             <td>${videos[i].durata} minuti</td>
                             <td>${videos[i].prezzo_giornaliero} €</td>
                             <td>${videos[i].tipo}</td> 
-                            <td class="${oCell.cssClass}">${oCell.availableCopies} su ${oCell.totalCopies} disponibili</td>
+                            <td class="${oAvailCopiesCell.cssClass}">${oAvailCopiesCell.availableCopies} su ${oAvailCopiesCell.totalCopies} disponibili</td>
                         </tr>`;
     }	
     html += `       </tbody>
@@ -102,8 +105,7 @@ function BuildVideosTableHead() {
 function formatAvailableCopiesCellObject(row) {
     var cssClass = "";
     var availableCopies = row.copie_totali - row.copie_danneggiate - row.copie_noleggiate;
-    var availableCopiesPercentage = (availableCopies / (parseInt(row.copie_totali) + 2)) * 100;
-    console.log(availableCopiesPercentage);
+    var availableCopiesPercentage = (availableCopies / (parseInt(row.copie_totali) + 2)) * 100;;
     if(availableCopiesPercentage <= 50 && availableCopiesPercentage > 25) {
         cssClass = "alert alert-warning";
     } else if (availableCopiesPercentage <= 25) {
@@ -114,6 +116,15 @@ function formatAvailableCopiesCellObject(row) {
         totalCopies: row.copie_totali,
         cssClass: cssClass
     };
+}
+
+function formatCastCell(cast) {
+    var cellRender = "";
+    for(var i = 0; i < cast.length; i++) {
+        cellRender += `${cast[i].nome_attore} ${cast[i].cognome_attore}, `;
+    }
+    cellRender = cellRender.trim().substring(0, cellRender.lastIndexOf(","));
+    return cellRender;
 }
 
 function attachCollapseRowEvent() {
@@ -144,13 +155,77 @@ function formatCollapsedDetails (row) {
                     </tr>
                     <tr>
                         <td>Attori</td>
-                        <td>`;
-                    for(var i = 0; i < row.cast.length; i++) {
-                        html += `${row.cast[i].nome_attore} ${row.cast[i].cognome_attore}, `;
-                    }
-                        `</td>
+                        <td>${row.cast}</td>
                     </tr>`;
     html +=     `</table>`;
+    return html;
+}
+
+/* ACTIONS */
+/* Rent Video Action */
+function rentVideoAction(e, dt, node, config) {
+    var row = dt.rows({ selected: true }).data()[0];    
+    var editModalBody = buildCustomerForm(row);
+
+    modalOptions = {
+        title: "Noleggia film",
+        body: editModalBody,
+        cancelButton: {
+            text: "Annulla"
+        },
+        confirmButton: {
+            text: "Conferma noleggio",
+            action: formClickDelegate
+        }
+    }
+    modal = new Modal(modalOptions);
+    modal.open();  
+    loadSelect(row);
+}
+
+function rentVideo() {
+    var fileInput = $("#CustomerForm_liberatoria");
+    var customer = getCustomerFromForm();
+    var files = [];
+    if(fileInput.length > 0) {
+        files = $("#CustomerForm_liberatoria")[0].files;
+    } else {
+        customer.keepExistingFile = true;
+    }
+    customersManagementService.editCustomer(customer, files[0])
+        .done(actionSuccess)
+        .fail(restCallError);
+}
+
+/* Actions shared functions  */
+function buildCustomerForm(row) {
+    var isEditForm = row != undefined;
+    var html = `<form class="form-signin" onsubmit="${isEditForm ? "editItem()" : "insertItem()"};return false;">
+                    <input id="CustomerForm_id_cliente" type="hidden" value="${isEditForm ? row.id_cliente : ""}">
+                    <label for="CustomerForm_nome" class="mt-2">Nome</label>
+                    <input id="CustomerForm_nome" type="text" class="form-control" value="${isEditForm ? row.nome : ""}" text="${isEditForm ? row.nome : ""}" required>
+                    <label for="CustomerForm_cognome" class="mt-2">Cognome</label>
+                    <input id="CustomerForm_cognome" type="text" class="form-control" value="${isEditForm ? row.cognome : ""}" text="${isEditForm ? row.cognome : ""}" required>
+                    <label for="CustomerForm_indirizzo" class="mt-2">Indirizzo</label>
+                    <input id="CustomerForm_indirizzo" type="text" class="form-control" value="${isEditForm ? row.indirizzo : ""}" text="${isEditForm ? row.indirizzo : ""}" required>
+                    <label for="CustomerForm_telefono_cellulare" class="mt-2">Cellulare</label>
+                    <input id="CustomerForm_telefono_cellulare" type="number" class="form-control" value="${isEditForm ? row.telefono_cellulare : ""}" text="${isEditForm ? row.telefono_cellulare : ""}" required>
+                    <label for="CustomerForm_telefono_casa" class="mt-2">Telefono fisso</label>
+                    <input id="CustomerForm_telefono_casa" type="number" class="form-control" value="${isEditForm ? row.telefono_casa : ""}" text="${isEditForm ? row.telefono_casa : ""}">
+                    <label for="CustomerForm_email" class="mt-2">Email</label>
+                    <input id="CustomerForm_email" type="text" class="form-control" value="${isEditForm ? row.email : ""}" text="${isEditForm ? row.email : ""}" required>
+                    <label for="CustomerForm_data_nascita" class="mt-2">Data di nascita</label>
+                    <input id="CustomerForm_data_nascita" type="date" class="form-control" value="${isEditForm ? switchDateDigitsPosition(row.data_nascita) : ""}" text="${isEditForm ? switchDateDigitsPosition(row.data_nascita) : ""}" required>
+                    <label for="CustomerForm_liberatoria" class="mt-2">Liberatoria</label>
+                    <div id="CustomerForm_fileInputContainer" class="row px-3">
+                        ${isEditForm && row.liberatoria != "null" ? buildLiberatoriaDynamicTag(row) : `<input id="CustomerForm_liberatoria" type="file" class="form-control" accept="pdf">`}
+                    </div>
+                    <label for="CustomerForm_fidelizzazione" class="mt-2">Tipo fidelizzazione</label>
+                    <div id="CustomerForm_fidelizzazione_container">
+                        <select id="CustomerForm_fidelizzazione" class="form-control"></select>
+                    </div>
+                    <button id="CustomerForm_insert_button" class="d-none" type="submit">
+                </form>`;
     return html;
 }
 
--- php/RentalManagementService.php
+++ php/RentalManagementService.php
@@ -83,12 +83,46 @@ class RentalManagementService {
         return $a->id_film > $b->id_film;
     }
 
+    function GetMostRecentCopies() {
+        Logger::Write("Processing ". __FUNCTION__ ." request.", $GLOBALS["CorrelationID"]);
+        TokenGenerator::ValidateToken();
+        $filters = json_decode($_POST["filters"]);
+        $query = 
+            "SELECT fi.id_film, fi.titolo, fi.durata, fi.prezzo_giornaliero,
+                    ge.tipo, cp.nome as casa_produttrice_nome, re.nome as regista_nome, re.cognome as regista_cognome,
+                    count(*) as copie_totali, SUM(co.noleggiato) as copie_noleggiate, SUM(co.danneggiato) as copie_danneggiate
+            FROM copia co
+            INNER JOIN film fi
+            ON co.id_film = fi.id_film
+            INNER JOIN genere ge
+            ON fi.id_genere = ge.id_genere
+            INNER JOIN casa_produttrice cp
+            ON fi.id_casa_produttrice = cp.id_casa_produttrice
+            INNER JOIN regista re
+            ON fi.id_regista = re.id_regista            
+            WHERE co.id_punto_vendita = %d
+            AND co.restituito = 0
+            GROUP BY fi.id_film";
+        $query = sprintf($query, $filters->id_punto_vendita);
+        Logger::Write("Query: ".$query, $GLOBALS["CorrelationID"]);
+        $res = self::ExecuteQuery($query);
+        $videosArray = array();
+        while($row = $res->fetch_assoc()){
+            // $video = new Video($row);
+            $videosArray[] = $row;
+        }
+        
+    }
+
     // Switcha l'operazione richiesta lato client
     function Init() {
         try {
             switch($_POST["action"]) {
                 case "getVideosInStorageWithCount":
                     self::GetVideosInStorageWithCount();
+                    break;
+                case "getMostRecentCopies":
+                    self::GetMostRecentCopies();
                     break;
                 default: 
                     exit(json_encode($_POST));
